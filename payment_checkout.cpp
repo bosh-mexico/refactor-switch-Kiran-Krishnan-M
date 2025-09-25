6e30084478b6baa11abf2d0f96707f4164eef8a4
Kiran Krishnan M <krishnankiran886@gmail.com>
2025-09-25 19:28:40 +0530

    Update payment_checkout.cpp

--- payment_checkout.cpp
+++ payment_checkout.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <iomanip> // For formatting the amount
-#include <string>
+#include <iomanip>
 using namespace std;
 
 // Enum for Payment Modes
@@ -11,40 +10,78 @@ enum class PaymentMode {
     Unknown
 };
 
-// Checkout function
-void checkout(PaymentMode mode, double amount) {
-    cout << fixed << setprecision(2); // Format amount to 2 decimal places
+// Base Processor Interface
+class Processor {
+public:
+    virtual void process(double amount) = 0;
+    virtual ~Processor() = default;
+};
 
-    switch (mode) {
-        case PaymentMode::PayPal:
-            cout << "Processing PayPal payment of $" << amount << endl;
-            // TODO: Integrate PayPal API here
-            break;
-
-        case PaymentMode::GooglePay:
-            cout << "Processing GooglePay payment of $" << amount << endl;
-            // TODO: Integrate GooglePay API here
-            break;
-
-        case PaymentMode::CreditCard:
-            cout << "Processing Credit Card payment of $" << amount << endl;
-            // TODO: Integrate Credit Card API here
-            break;
-
-        default:
-            cout << "Error: Invalid payment mode selected!" << endl;
-            break;
+// PayPal Processor
+class PayPalProcessor : public Processor {
+public:
+    void process(double amount) override {
+        cout << "Processing PayPal payment of $" << fixed << setprecision(2) << amount << endl;
     }
-}
+};
 
-// Example usage
+// GooglePay Processor
+class GooglePayProcessor : public Processor {
+public:
+    void process(double amount) override {
+        cout << "Processing GooglePay payment of $" << fixed << setprecision(2) << amount << endl;
+    }
+};
+
+// CreditCard Processor
+class CreditCardProcessor : public Processor {
+public:
+    void process(double amount) override {
+        cout << "Processing Credit Card payment of $" << fixed << setprecision(2) << amount << endl;
+    }
+};
+
+// Unknown Processor
+class UnknownProcessor : public Processor {
+public:
+    void process(double amount) override {
+        cout << "Error: Invalid payment mode selected!" << endl;
+    }
+};
+
+// Factory to create processors
+class ProcessorFactory {
+public:
+    static Processor* createProcessor(PaymentMode mode) {
+        switch (mode) {
+            case PaymentMode::PayPal:
+                return new PayPalProcessor();
+            case PaymentMode::GooglePay:
+                return new GooglePayProcessor();
+            case PaymentMode::CreditCard:
+                return new CreditCardProcessor();
+            default:
+                return new UnknownProcessor();
+        }
+    }
+};
+
+// Main function
 int main() {
     double amount = 150.75;
 
-    checkout(PaymentMode::PayPal, amount);
-    checkout(PaymentMode::GooglePay, amount);
-    checkout(PaymentMode::CreditCard, amount);
-    checkout(PaymentMode::Unknown, amount);
+    PaymentMode modes[] = {
+        PaymentMode::PayPal,
+        PaymentMode::GooglePay,
+        PaymentMode::CreditCard,
+        PaymentMode::Unknown
+    };
+
+    for (auto mode : modes) {
+        Processor* processor = ProcessorFactory::createProcessor(mode);
+        processor->process(amount);
+        delete processor;
+    }
 
     return 0;
 }
